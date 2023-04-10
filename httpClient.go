fdb2c3621477b7ba6807730e392aee5c20ab5449
asadzeynal <asadzeynal9@gmail.com>
2023-04-10 22:11:40 +0400

    fix: removed main from controller, added errors

--- httpClient.go
+++ httpClient.go
@@ -19,15 +19,10 @@ func NewClient() *Client {
 	}
 }
 
-func main() {
-	c := NewClient()
-	c.FetchDataByInn("7840473679")
-}
-
-func (c *Client) FetchDataByInn(inn string) CompanyData {
+func (c *Client) FetchDataByInn(inn string) (CompanyData, error) {
 	req, err := http.NewRequest(http.MethodGet, "https://www.rusprofile.ru/search", nil)
 	if err != nil {
-		fmt.Printf("client: could not create request: %s\n", err)
+		return CompanyData{}, fmt.Errorf("client: could not create request: %w", err)
 	}
 
 	q := req.URL.Query()
@@ -36,17 +31,17 @@ func (c *Client) FetchDataByInn(inn string) CompanyData {
 
 	res, err := c.httpClient.Do(req)
 	if err != nil {
-		fmt.Printf("client: error making http request: %s\n", err)
+		return CompanyData{}, fmt.Errorf("client: error making http request: %w", err)
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		log.Printf("status code error: %d %s", res.StatusCode, res.Status)
+		return CompanyData{}, fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
 	}
 
 	data := parseResponse(res)
 	fmt.Println(data)
-	return data
+	return data, nil
 }
 
 func parseResponse(res *http.Response) CompanyData {
